5228d0ba2b8c79f82c328b70eddba557624fea24
Funmi Ojo <oluwafunmi.ojo@gmail.com>
2019-03-16 16:57:05 -0400

    Added get users get route and test

--- server/api/users.js
+++ server/api/users.js
@@ -1,6 +1,14 @@
 const router = require('express').Router()
 const { User } = require('../../db/models')
 
+router.get('/', async (req, res, next) => {
+  try {
+    res.send(await User.getAllUsers())
+  } catch (error) {
+    next(error)
+  }
+})
+
 router.post('/', async (req, res, next) => {
   try {
     const { spotifyId, email } = req.body
--- tests/api/users.spec.js
+++ tests/api/users.spec.js
@@ -2,10 +2,28 @@ const { expect } = require('chai')
 const request = require('supertest')
 const server = require('../../server')
 const db = require('../../db')
+const { User } = require('../../db/models')
 const { getUser } = require('../utils')
 
 describe.only('User routes', () => {
   beforeEach(() => db.sync({ force: true }))
+
+  describe('GET api/users', () => {
+    it('returns all users', async () => {
+      const user1 = await User.create(getUser())
+      const user2 = await User.create(getUser())
+      const response = await request(server).get('/api/users')
+      expect(response.statusCode).to.equal(200)
+      expect(response.body.length).to.equal(2)
+      expect(
+        response.body.filter(
+          user =>
+            user.spotifyId === user1.spotifyId ||
+            user.spotifyId === user2.spotifyId
+        ).length
+      ).to.equal(2)
+    })
+  })
 
   describe('POST api/users', () => {
     it('creates a new user', async () => {
